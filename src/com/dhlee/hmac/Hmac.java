c44c1ed0819fe8a5f61053bf2d69db355a54a9e6
rxnzero <rxnzero#gmail.com>
2020-12-03 11:26:21 +0900

    edit

--- src/com/dhlee/hmac/Hmac.java
+++ src/com/dhlee/hmac/Hmac.java
@@ -1,37 +1,44 @@
 package com.dhlee.hmac;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.NoSuchAlgorithmException;
-import java.security.InvalidKeyException;
  
 public class Hmac {
-    // hash 알고리즘 선택
-    private static final String ALGOLISM = "HmacSHA256";
+    private static final String ALGORISM = "HmacSHA256";
     
-    public static String hget(String message, String key) {
+    public static String generate(String message, String key) throws Exception {
         try {
-            // hash 알고리즘과 암호화 key 적용
-            Mac hasher = Mac.getInstance(ALGOLISM);
-            hasher.init(new SecretKeySpec(key.getBytes(), ALGOLISM));
-            // messages를 암호화 적용 후 byte 배열 형태의 결과 리턴
+            Mac hasher = Mac.getInstance(ALGORISM);
+            SecretKeySpec secretSpec = new SecretKeySpec(key.getBytes("UTF-8"), ALGORISM);
+            hasher.init(secretSpec);
             byte[] hash = hasher.doFinal(message.getBytes());
             return byteToString(hash);
         }
         catch (NoSuchAlgorithmException e){
             e.printStackTrace();
+            throw e;
         }
         catch (InvalidKeyException e){
             e.printStackTrace();
+            throw e;
         }
-        return "";
+        catch (Exception e){
+            e.printStackTrace();
+            throw e;
+        }
     }
  
-    // byte[]의 값을 16진수 형태의 문자로 변환하는 함수
-    private static String byteToString(byte[] hash) {
+    private static String byteToString(byte[] bytes) {
+    	return byteToString2(bytes);
+    }
+    
+    private static String byteToString1(byte[] bytes) {
         StringBuffer buffer = new StringBuffer();
-        for (int i = 0; i < hash.length; i++) {
-            int d = hash[i];
+        for (int i = 0; i < bytes.length; i++) {
+            int d = bytes[i];
             d += (d < 0)? 256 : 0;
             if (d < 16) {
                 buffer.append("0");
@@ -39,5 +46,13 @@ public class Hmac {
             buffer.append(Integer.toString(d, 16));
         }
         return buffer.toString();
+    }
+    
+    private static String byteToString2(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for(byte b: bytes) {
+        	sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
--- src/com/dhlee/hmac/HmacTest.java
+++ src/com/dhlee/hmac/HmacTest.java
@@ -9,7 +9,7 @@ public class HmacTest {
 	public static void main(String[] argv) {
 		String key = "testkey";
 		String hmacData = "data ¿‘¥œ¥Ÿ.";
-		String hash = Hmac.hget(hmacData, key);
+		String hash = Hmac.generate(hmacData, key);
 		System.out.println("Hash size = " + hash.length());
 		System.out.println("Hash value [" + hash+"]");
 	}
